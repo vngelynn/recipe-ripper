c0891c09ac59eafce429585e73a2c566621f47ef
Angelynn <89512890+vngelynn@users.noreply.github.com>
2026-07-16 16:19:08 -0700

    stylistic reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState } from "react"
 import UrlForm from "../components/UrlForm"
+import RecipePreview from "@/components/RecipePreview"
 
 export default function Home() {
   const [recipeUrl, setRecipeUrl] = useState("")
@@ -31,13 +32,14 @@ export default function Home() {
   }
 
   return (
-    <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
+    <div className='flex flex-col flex-1 items-center justify-center font-sans'>
+      <main className='flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start'>
         <UrlForm
           updateRecipeUrl={setRecipeUrl}
           url={recipeUrl}
           getRecipe={checkRecipe}
         />
+        <RecipePreview />
       </main>
     </div>
   )
