e31b1bc3e91404bc928de0a0d40ae04ad5278e36
Angelynn <89512890+vngelynn@users.noreply.github.com>
2026-07-17 10:44:36 -0700

    set up recipe preview

--- src/components/RecipePreview.tsx
+++ src/components/RecipePreview.tsx
@@ -1,7 +1,47 @@
-export default function RecipePreview() {
+import RecipeDetails from "./RecipeDetails"
+
+// TODO: export types to types.ts
+interface HowToStep {
+  "@type": "HowToStep"
+  text: string
+  name: string
+  url: string
+}
+interface recipe {
+  name: string
+  ingredients: string[]
+  instructions: HowToStep[]
+  image: string
+  servings: string[]
+  prepTime: string
+  cookTime: string
+  totalTime: string
+}
+
+export default function RecipePreview(recipe: recipe) {
+  const {
+    name,
+    ingredients,
+    instructions,
+    image,
+    servings,
+    prepTime,
+    cookTime,
+    totalTime,
+  } = recipe
+
   return (
     <div>
-      <h1>Preview Recipe</h1>
+      <div>
+        CAUTION! this is a temporary preview, if you'd like to save, click here!
+        <h1>
+          <i>{name}</i>
+        </h1>
+        {/* TODO: add get source logic to display clipped from {$} */}
+      </div>
+      <div>
+        <RecipeDetails />
+      </div>
     </div>
   )
 }
